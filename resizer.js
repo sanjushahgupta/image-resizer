3d10949ed0afe1652736ee2b34c22ad1aa67d764
Sanju Shah <sanjushahgupta@gmail.com>
2024-01-19 17:22:26 +0100

    chores: refactor

---
+++ resizer.js
@@ -0,0 +1,72 @@
+const sharp = require("sharp");
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+const s3Client = new S3Client({
+  region: "eu-central-1",
+  forcePathStyle: true,
+});
+const myImageBucket = "image-bucket-535";
+
+async function getObjectFromS3(objectKey) {
+  const getObjectParams = {
+    Bucket: myImageBucket,
+    Key: objectKey,
+  };
+
+  try {
+    const response = await s3Client.send(new GetObjectCommand(getObjectParams));
+    const Imagebuffer = await streamToBuffer(response.Body);
+    return Imagebuffer;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function streamToBuffer(stream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("end", () => resolve(Buffer.concat(chunks)));
+    stream.on("error", reject);
+  });
+}
+
+async function resizeImage(imageBuffer) {
+  let transform = sharp(imageBuffer);
+  transform = transform.resize({
+    width: 150,
+  });
+
+  return await transform.toBuffer();
+}
+
+async function uploadToS3(file, fileName) {
+  let putObjectParams;
+  if (!file) {
+    throw Error("File not found");
+  }
+
+  putObjectParams = {
+    Bucket: myImageBucket,
+    Key: fileName,
+    Body: file,
+  };
+
+  try {
+    const putObjectCmd = new PutObjectCommand(putObjectParams);
+    await s3Client.send(putObjectCmd);
+    return;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getObjectFromS3,
+  resizeImage,
+  uploadToS3,
+};
--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require("express");
-const sharp = require("sharp");
-const {
-  S3Client,
-  PutObjectCommand,
-  GetObjectCommand,
-} = require("@aws-sdk/client-s3");
+const { getObjectFromS3, resizeImage, uploadToS3 } = require("./resizer.js");
 
 const server = express();
-const s3Client = new S3Client({
-  region: "eu-central-1",
-  forcePathStyle: true,
-});
-const myImageBucket = "image-bucket-535";
 
 server.get("/resize", async (req, res) => {
   const imageToResize = req.query.image;
@@ -39,60 +29,6 @@ server.get("/resize", async (req, res) => {
     res.send(error);
   }
 });
-
-async function resizeImage(imageBuffer) {
-  let transform = sharp(imageBuffer);
-  transform = transform.resize({
-    width: 150,
-  });
-
-  return await transform.toBuffer();
-}
-
-async function getObjectFromS3(objectKey) {
-  const getObjectParams = {
-    Bucket: myImageBucket,
-    Key: objectKey,
-  };
-
-  try {
-    const response = await s3Client.send(new GetObjectCommand(getObjectParams));
-    const Imagebuffer = await streamToBuffer(response.Body);
-    return Imagebuffer;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function streamToBuffer(stream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-    stream.on("error", reject);
-  });
-}
-
-async function uploadToS3(file, fileName) {
-  let putObjectParams;
-  if (!file) {
-    throw Error("File not found");
-  }
-
-  putObjectParams = {
-    Bucket: myImageBucket,
-    Key: fileName,
-    Body: file,
-  };
-
-  try {
-    const putObjectCmd = new PutObjectCommand(putObjectParams);
-    await s3Client.send(putObjectCmd);
-    return;
-  } catch (error) {
-    throw error;
-  }
-}
 
 server.listen(3000, () => {
   console.log("Server started");
